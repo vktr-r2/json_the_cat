d01c6ebe3ae1230826890ccb0ac7b501ec9ef288
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-13 19:39:05 -0400

    Update breedFetcher.js to only return Description of first cat returned

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,11 +3,15 @@ const catQuery = (process.argv.slice(2, process.argv.length));
 
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${catQuery}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+  }
   if (response.statusCode !== 200) console.log('statusCode:', response && response.statusCode);   //Handle response failure
   if (body.length === 2) {   //Handle cat not found (not sure why its 2?? and return)
     console.log("Cat breed match not found");
     return;
   }
-  console.log('body:', JSON.parse(body)); // Print the result of the GET call based on catQuery input
+  let catinfo = JSON.parse(body);
+  console.log(catinfo[0]["description"]);
+  //console.log(catinfo[0][description]); // Print the result of the GET call based on catQuery input
 });
